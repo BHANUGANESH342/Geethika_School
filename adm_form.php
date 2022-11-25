f57a99c8498a0372e9cb0ad4413c5fc28312e04a
Ravi Kumar <ravikumar_csd@srkrec.edu.in>
2022-11-25 13:53:26 +0530

    small error

--- adm_form.php
+++ adm_form.php
@@ -214,7 +214,8 @@ if (isset($_POST["submit"]))
     include("footer.php");
         ?>
         
-        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></><script src="assets/js/jquery.min.js"></script>
+        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+        <script src="assets/js/jquery.min.js"></script>
         
         <script src="assets/js/bootstrap.bundle.min.js"></script>
         
