9a0a26b0fa07aeda283e751814dafe60a68bffb4
Ravi Kumar <ravikumar_csd@srkrec.edu.in>
2022-12-02 22:44:32 +0530

    Index of admin page

---
+++ admin/footer.php
@@ -0,0 +1,92 @@
+
+<div class="footer-area pt-100 pb-70">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-sm-6">
+<div class="footer-logo-area">
+<a href="index.html"><img src="../school images/main white.png" alt="Image"></a>
+<p>Geethika School aims to be recognized as an accomplished, comprehensive academic foundation that helps budding geniuses conform to the demands of a vigorous world.</p>
+<div class="contact-list">
+<ul>
+<li><a href="tel:+01987655567685">+91 90148 28382
+</a></li>
+<li><a href="/cdn-cgi/l/email-protection#d3b2b7bebabd93a0b2bda6fdb6b7a6"><span class="__cf_email__" data-cfemail="7617121b1f18360517180358131203"></span></a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3 >What we are...</h3>
+<div class="list">
+<ul>
+<li><a href="visionary.php">Our Visionaries</a></li>
+<li><a href="mission.php">Our Vission & Mission</a></li>
+<li><a href="leaders.php">The Leaders</a></li>
+<!-- <li><a href="campus-life.html">Housing</a></li>
+<li><a href="campus-life.html">Information Technologies</a></li>
+<li><a href="campus-life.html">Student Life</a></li> -->
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3>Our Campus</h3>
+<div class="list">
+<ul>
+<li><a href="academics.php">Acedemics</a></li>
+<li><a href="activities.php">Cirricular Activities</a></li>
+<li><a href="gallery.php">Gallery</a></li>
+<li><a href="adm_guideline.php">Guidelines</a></li>
+<li><a href="adm_form.php">Admission Form</a></li>
+<!-- <li><a href="campus-life.html">Human Resources</a></li> -->
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-sm-6">
+<div class="footer-widjet">
+<h3>Contact Us</h3>
+<div class="list">
+<ul>
+<li><a>Geethika School</a></li>
+<li><a>HYD Highway</a></li>
+<li><a>Brahmanapalli-522437</a></li>
+<li><a>Piduguralla Mandal</a></li>
+<li><a>Guntur</a></li>
+<li><a>Andhra Pradesh</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<!-- <div class="shape">
+<img src="assets/images/shape-1.png" alt="Image">
+</div> -->
+</div>
+</div>
+
+
+<div class="copyright-area">
+<div class="container">
+<div class="copyright">
+<div class="row">
+
+<div class="col-lg-6 col-md-8">
+<div class="copy">
+<p>©  <a href="#" target="_blank">Geethika School 2022</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="go-top">
+<i class="ri-arrow-up-s-line"></i>
+<i class="ri-arrow-up-s-line"></i>
+</div>
+
+
