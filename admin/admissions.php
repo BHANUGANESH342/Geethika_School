a9ae7b7f141a5b7f7811b27b596c2977b36f0a1d
Pranav Teja <pranav_mcr@srkrec.edu.in>
2022-12-03 15:47:38 +0530

    admin

---
+++ admin/admissions.php
@@ -0,0 +1,227 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="../assets/css/meanmenu.css">
+
+<link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+
+<link rel="stylesheet" href="../assets/css/owl.theme.default.min.css">
+
+<link rel="stylesheet" href="../assets/css/magnific-popup.css">
+
+<link rel="stylesheet" href="../assets/css/flaticon.css">
+
+<link rel="stylesheet" href="../assets/css/remixicon.css">
+
+<link rel="stylesheet" href="../assets/css/odometer.min.css">
+
+<link rel="stylesheet" href="../assets/css/aos.css">
+
+<link rel="stylesheet" href="../assets/css/style.css">
+
+<link rel="stylesheet" href="../assets/css/dark.css">
+
+<link rel="stylesheet" href="../assets/css/responsive.css">
+<link rel="icon" type="image/png" href="../assets/images/favicon.png">
+<title>Sanu - College University HTML Template</title>
+</head>
+<body>
+
+<div class="preloader-area">
+<div class="spinner">
+<div class="inner">
+<div class="disc"></div>
+<div class="disc"></div>
+<div class="disc"></div>
+</div>
+</div>
+</div>
+
+
+<?php include'header.php';?>
+
+
+
+<div class="courses-area pt-100 pb-70">
+<div class="container">
+<div class="row">
+<div class="col-lg-8">
+<div class="courses-left-content">
+<div class="row">
+<div class="col-lg-6 col-md-6">
+<div class="single-courses-card style4">
+<div class="courses-img">
+<a href="courses-details.html"><img src="../assets/images/courses/courses-1.jpg" alt="Image"></a>
+</div>
+<div class="courses-content">
+<div class="admin-profile">
+<img src="../assets/images/courses/admin-1.jpg" alt="Image">
+<p>With <a href="courses-details.html">Jessica Hamson</a></p>
+</div>
+<a href="courses-details.html"><h3>Python Programming For Data Science</h3></a>
+<div class="bottom-content">
+<ul class="d-flex justify-content-between">
+<li>
+<ul>
+<li><i class="flaticon-graduation"></i>321</li>
+<li><i class="flaticon-bubble-chat"></i>1.2k</li>
+</ul>
+</li>
+<li><a href="#">Free</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="footer-area pt-100 pb-70">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-sm-6">
+<div class="footer-logo-area">
+<a href="index.html"><img src="../assets/images/white-logo.png" alt="Image"></a>
+<p>Sanu University was established by J.H Merthon in 1810 for the public benefit. Afterwards, it is recognized globally</p>
+<div class="contact-list">
+ <ul>
+<li><a href="tel:+01987655567685">+01-9876-5556-7685
+</a></li>
+<li><a href="/cdn-cgi/l/email-protection#d6b7b2bbbfb896a5b7b8a3f8b3b2a3"><span class="__cf_email__" data-cfemail="5d3c393034331d2e3c332873383928">[email&#160;protected]</span></a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3>Campus Life</h3>
+<div class="list">
+<ul>
+<li><a href="campus-life.html">Accessibility</a></li>
+<li><a href="campus-life.html">Financial Aid</a></li>
+<li><a href="campus-life.html">Food Services</a></li>
+<li><a href="campus-life.html">Housing</a></li>
+<li><a href="campus-life.html">Information Technologies</a></li>
+<li><a href="campus-life.html">Student Life</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3>Our Campus</h3>
+<div class="list">
+<ul>
+<li><a href="campus-life.html">Acedemic</a></li>
+<li><a href="campus-life.html">Planning & Administration</a></li>
+<li><a href="campus-life.html">Campus Safety</a></li>
+<li><a href="campus-life.html">Office of the Chancellor</a></li>
+<li><a href="campus-life.html">Facility Services</a></li>
+<li><a href="campus-life.html">Human Resources</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-sm-6">
+<div class="footer-widjet">
+<h3>Academics</h3>
+<div class="list">
+<ul>
+<li><a href="academics.html">Canvas</a></li>
+<li><a href="academics.html">Catalyst</a></li>
+<li><a href="academics.html">Library</a></li>
+<li><a href="academics.html">Time Schedule</a></li>
+<li><a href="academics.html">Apply For Admissions</a></li>
+<li><a href="academics.html">Pay My Tuition</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="shape">
+<img src="../assets/images/shape-1.png" alt="Image">
+</div>
+</div>
+</div>
+
+
+<div class="copyright-area">
+<div class="container">
+<div class="copyright">
+<div class="row">
+<div class="col-lg-6 col-md-4">
+<div class="social-content">
+<ul>
+<li><span>Follow Us On</span></li>
+<li>
+<a href="https://www.facebook.com" target="_blank"><i class="ri-facebook-fill"></i></a>
+</li>
+<li>
+<a href="https://www.twitter.com" target="_blank"><i class="ri-twitter-fill"></i></a>
+</li>
+<li>
+<a href="https://instagram.com/?lang=en" target="_blank"><i class="ri-instagram-line"></i></a>
+</li>
+<li>
+<a href="https://linkedin.com/?lang=en" target="_blank"><i class="ri-linkedin-fill"></i></a>
+</li>
+</ul>
+</div>
+</div>
+<div class="col-lg-6 col-md-8">
+<div class="copy">
+<p>© Sanu is Proudly Owned by <a href="https://hibootstrap.com/" target="_blank">HiBootstrap</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="go-top">
+<i class="ri-arrow-up-s-line"></i>
+<i class="ri-arrow-up-s-line"></i>
+</div>
+
+
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="../assets/js/jquery.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/js/jquery.meanmenu.js"></script>
+
+<script src="../assets/js/owl.carousel.min.js"></script>
+
+<script src="../assets/js/carousel-thumbs.min.js"></script>
+
+<script src="../assets/js/jquery.magnific-popup.js"></script>
+
+<script src="../assets/js/aos.js"></script>
+
+<script src="../assets/js/odometer.min.js"></script>
+
+<script src="../assets/js/appear.min.js"></script>
+
+<script src="../assets/js/form-validator.min.js"></script>
+
+<script src="../assets/js/contact-form-script.js"></script>
+
+<script src="../assets/js/ajaxchimp.min.js"></script>
+
+<script src="../assets/js/custom.js"></script>
+</body>
+</html>
---
+++ admin/header.php
@@ -0,0 +1,281 @@
+
+<div class="top-header-area">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-6 col-md-6">
+<div class="header-left-content">
+<p>Get the latest updates and Sanu's response to COVID-19</p>
+</div>
+</div>
+<div class="col-lg-6 col-md-6">
+<div class="header-right-content">
+<div class="list">
+<ul>
+<li><a href="graduate-admission.html">Students</a></li>
+<li><a href="campus-life.html">Faculty & Staff</a></li>
+<li><a href="admission.html">Visitors</a></li>
+<li><a href="academics.html">Academics</a></li>
+<li><a href="alumni.html">Alumni</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="navbar-area nav-bg-1">
+<div class="mobile-responsive-nav">
+<div class="container">
+<div class="mobile-responsive-menu">
+<div class="logo">
+<a href="index.html">
+<img src="../assets/images/logo.png" class="main-logo" lt="logo">
+<img src="../assets/images/white-logo.png" class="white-logo" alt="logo">
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="desktop-nav">
+<div class="container">
+<nav class="navbar navbar-expand-md navbar-light">
+<a class="navbar-brand" href="index.html">
+<img src="../assets/images/white-logo.png" alt="logo">
+</a>
+<div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+<ul class="navbar-nav ms-auto">
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Home
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="index.html" class="nav-link">Home One</a>
+</li>
+<li class="nav-item">
+<a href="index-2.html" class="nav-link">Home Two</a>
+</li>
+<li class="nav-item">
+<a href="index-3.html" class="nav-link">Home Three</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle active">
+Pages
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="graduate-admission.html" class="nav-link">Graduate Admission</a>
+</li>
+<li class="nav-item">
+<a href="campus-life.html" class="nav-link">Campus Life</a>
+</li>
+<li class="nav-item">
+<a href="alumni.html" class="nav-link">Alumni</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle active">
+Academics
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="academics.html" class="nav-link active">Academics</a>
+</li>
+<li class="nav-item">
+<a href="academics-details.html" class="nav-link">Academics Details</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Latest News
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="latest-news.html" class="nav-link">Our Latest News</a>
+</li>
+<li class="nav-item">
+<a href="news-details.html" class="nav-link">News Details</a>
+ </li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="faq.html" class="nav-link">FAQ</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Users
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="login.html" class="nav-link">Login</a>
+</li>
+<li class="nav-item">
+<a href="register.html" class="nav-link">Register</a>
+</li>
+<li class="nav-item">
+<a href="recover-password.html" class="nav-link">Recover Password</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
+</li>
+<li class="nav-item">
+<a href="terms-conditions.html" class="nav-link">Terms And Conditions</a>
+</li>
+<li class="nav-item">
+<a href="coming-soon.html" class="nav-link">Coming Soon</a>
+</li>
+<li class="nav-item">
+<a href="404.html" class="nav-link">404 Page</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Courses
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="courses.html" class="nav-link">Courses</a>
+</li>
+<li class="nav-item">
+<a href="courses-details.html" class="nav-link">Courses Details</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Health Care
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="health-care.html" class="nav-link">Health Care</a>
+</li>
+<li class="nav-item">
+<a href="health-care-details.html" class="nav-link">Health Care Details</a>
+</li>
+ </ul>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link dropdown-toggle">
+Events
+</a>
+<ul class="dropdown-menu">
+<li class="nav-item">
+<a href="events.html" class="nav-link">Events</a>
+</li>
+<li class="nav-item">
+<a href="events-details.html" class="nav-link">Events Details</a>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="contact-us.html" class="nav-link">Contact Us</a>
+</li>
+</ul>
+<div class="others-options">
+<div class="icon">
+<i class="ri-menu-3-fill" data-bs-toggle="modal" data-bs-target="#sidebarModal"></i>
+</div>
+</div>
+</div>
+</nav>
+</div>
+</div>
+<div class="others-option-for-responsive">
+<div class="container">
+<div class="dot-menu">
+<div class="inner">
+<div class="icon">
+<i class="ri-menu-3-fill" data-bs-toggle="modal" data-bs-target="#sidebarModal"></i>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="sidebarModal modal right fade" id="sidebarModal" tabindex="-1" role="dialog">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<button type="button" class="close" data-bs-dismiss="modal"><i class="ri-close-line"></i></button>
+<div class="modal-body">
+<a href="index.html">
+<img src="../assets/images/logo.png" class="main-logo" alt="logo">
+<img src="../assets/images/white-logo.png" class="white-logo" alt="logo">
+</a>
+<div class="sidebar-content">
+<h3>About Us</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<div class="sidebar-btn">
+<a href="contact.html" class="default-btn">Let’s Talk</a>
+</div>
+</div>
+<div class="sidebar-contact-info">
+<h3>Contact Information</h3>
+<ul class="info-list">
+<li><i class="ri-phone-fill"></i> <a href="tel:9901234567">+990-123-4567</a></li>
+<li><i class="ri-mail-line"></i><a href="/cdn-cgi/l/email-protection#cba3aea7a7a48bb8aaa5bee5a8a4a6"><span class="__cf_email__" data-cfemail="036b666f6f6c4370626d762d606c6e">[email&#160;protected]</span></a></li>
+<li><i class="ri-map-pin-line"></i> 413 North Las Vegas, NV 89032</li>
+</ul>
+</div>
+<ul class="sidebar-social-list">
+<li>
+<a href="https://www.facebook.com" target="_blank"><i class="flaticon-facebook"></i></a>
+</li>
+ <li>
+<a href="https://www.twitter.com" target="_blank"><i class="flaticon-twitter"></i></a>
+</li>
+<li>
+<a href="https://linkedin.com/?lang=en" target="_blank"><i class="flaticon-linkedin"></i></a>
+</li>
+<li>
+<a href="https://instagram.com/?lang=en" target="_blank"><i class="flaticon-instagram"></i></a>
+</li>
+</ul>
+<div class="contact-form">
+<h3>Ready to Get Started?</h3>
+<form id="contactForm">
+<div class="row">
+<div class="col-lg-12 col-md-6">
+<div class="form-group">
+<input type="text" name="name" class="form-control" required="" data-error="Please enter your name" placeholder="Your name">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-lg-12 col-md-6">
+<div class="form-group">
+<input type="email" name="email" class="form-control" required="" data-error="Please enter your email" placeholder="Your email address">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<div class="form-group">
+<input type="text" name="phone_number" class="form-control" required="" data-error="Please enter your phone number" placeholder="Your phone number">
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<div class="form-group">
+<textarea name="message" class="form-control" cols="30" rows="6" required="" data-error="Please enter your message" placeholder="Write your message..."></textarea>
+<div class="help-block with-errors"></div>
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<button type="submit" class="default-btn">Send Message<span></span></button>
+<div id="msgSubmit" class="h3 text-center hidden"></div>
+<div class="clearfix"></div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
