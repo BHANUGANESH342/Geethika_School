83e2407868a271946189857333327e23ce288f77
Pranav Teja <pranav_mcr@srkrec.edu.in>
2022-12-04 14:29:05 +0530

    admin

---
+++ admin/stdprofile.php
@@ -0,0 +1,488 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="../assets/css/meanmenu.css">
+
+<link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+
+<link rel="stylesheet" href="../assets/css/owl.theme.default.min.css">
+
+<link rel="stylesheet" href="../assets/css/magnific-popup.css">
+
+<link rel="stylesheet" href="../assets/css/flaticon.css">
+
+<link rel="stylesheet" href="../assets/css/remixicon.css">
+
+<link rel="stylesheet" href="../assets/css/odometer.min.css">
+
+<link rel="stylesheet" href="../assets/css/aos.css">
+
+<link rel="stylesheet" href="../assets/css/style.css">
+
+<link rel="stylesheet" href="../assets/css/dark.css">
+
+<link rel="stylesheet" href="../assets/css/responsive.css">
+<link rel="icon" type="image/png" href="../assets/images/favicon.png">
+<title>Sanu - College University HTML Template</title>
+</head>
+<body>
+
+<div class="preloader-area">
+<div class="spinner">
+<div class="inner">
+<div class="disc"></div>
+<div class="disc"></div>
+<div class="disc"></div>
+</div>
+</div>
+</div>
+
+
+
+<div class="courses-details-area pt-100 pb-70">
+<div class="container">
+ <div class="row">
+<div class="col-lg-8">
+<div class="courses-details">
+<div class="courses-card">
+<h2>Python Programming For Data Science</h2>
+<div class="img">
+<img src="../assets/images/courses/courses-4.jpg" alt="Image">
+</div>
+<div class="list">
+<ul>
+<li>
+<div class="teacher">
+<img src="../assets/images/courses/admin-1.jpg" alt="Image">
+<p>Teacher: <a href="#">Jessica Hamson</a></p>
+</div>
+</li>
+<li><i class="flaticon-clock"></i><span>Last Update:</span>September 29, 2021</li>
+</ul>
+</div>
+</div>
+<div class="description">
+<div class="container p-0">
+<nav>
+<div class="nav nav-tabs d-flex justify-content-between" id="nav-tab" role="tablist">
+<button class="nav-link active" id="nav-overview-tab" data-bs-toggle="tab" data-bs-target="#nav-overview" type="button" role="tab" aria-controls="nav-overview" aria-selected="true">Overview</button>
+<button class="nav-link" id="nav-curriculum-tab" data-bs-toggle="tab" data-bs-target="#nav-curriculum" type="button" role="tab" aria-controls="nav-curriculum" aria-selected="false">Curriculum</button>
+<button class="nav-link" id="nav-instructor-tab" data-bs-toggle="tab" data-bs-target="#nav-instructor" type="button" role="tab" aria-controls="nav-instructor" aria-selected="false">Instructor</button>
+<button class="nav-link" id="nav-reviews-tab" data-bs-toggle="tab" data-bs-target="#nav-reviews" type="button" role="tab" aria-controls="nav-reviews" aria-selected="false">Reviews</button>
+</div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+<div class="tab-pane fade show active" id="nav-overview" role="tabpanel" aria-labelledby="nav-overview-tab">
+<div class="overview">
+<div class="learn">
+<h3>What Will You Learn?</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud xerci tation ulla mco laboris nisi ut Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore </p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud xerci tation ulla mco laboris nisi ut</p>
+</div>
+<div class="overview-box">
+<h4>Visual Learning with theory</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="overview-box">
+<h4>Layton Basic Logical Program</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="overview-box">
+<h4>Error resolution and implementation</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+</div>
+</div>
+<div class="tab-pane fade" id="nav-curriculum" role="tabpanel" aria-labelledby="nav-curriculum-tab">
+<div class="curriculum">
+<h3>Starting With Sanu</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore et dolore magna aliqua. Ut enim ad minim ven iam quis nostrud xerci tation ulla mco laboris nisi ut Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut labore</p>
+<div class="curriculum-list">
+<ul>
+<li>Lesson1: Introduction Of Programming language ?<span>01 Hour</span></li>
+<li>Lesson2: Installation and Set-Up Guide ?<span>03 Hour</span></li>
+<li>Lesson3: Working with String, Boolean Functions ?<span>01 Hour</span></li>
+<li>Lesson4: Working on Form Controls ?<span>30 min</span></li>
+<li>Lesson5: Error Solutions and code debugging ?<span>01 Hour</span></li>
+<li>Lesson6: Working with live site.<span>01 Hour</span></li>
+<li>Module Handling<span>02 Hour</span></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="tab-pane fade" id="nav-instructor" role="tabpanel" aria-labelledby="nav-instructor-tab">
+<div class="row">
+<div class="instructor">
+<div class="row align-items-center">
+<div class="col-lg-5">
+<div class="instructor-image">
+<img src="../assets/images/courses/instructor.jpg" alt="Image">
+</div>
+</div>
+<div class="col-lg-7">
+<div class="instructor-content">
+<div class="name">
+<h3>Samantha Nicola</h3>
+<p>Instructor Of Economy</p>
+</div>
+<p class="mb-30">deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+<div class="quick-contact">
+<h4>Quick Contact</h4>
+<ul>
+<li><i class="ri-phone-line"></i><span>Phone Number:</span><a href="tel:8002162020">(800) 216 2020
+</a></li>
+<li><i class="ri-mail-line"></i><span>Email:</span><a href="tel:8002162020"><span class="__cf_email__" data-cfemail="feb7909891be9b869f938e929bd09d9193">[email&#160;protected]</span></a></li>
+</ul>
+</div>
+<div class="social">
+<ul>
+<li>
+<a href="https://instagram.com/?lang=en" target="_blank"><i class="ri-instagram-line"></i></a>
+</li>
+<li>
+<a href="https://linkedin.com/?lang=en" target="_blank"><i class="ri-linkedin-fill"></i></a>
+</li>
+<li>
+<a href="https://www.facebook.com" target="_blank"><i class="ri-facebook-fill"></i></a>
+</li>
+<li>
+<a href="https://www.twitter.com" target="_blank"><i class="ri-twitter-fill"></i></a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="tab-pane fade" id="nav-reviews" role="tabpanel" aria-labelledby="nav-reviews-tab">
+<div class="reviews">
+<div class="single-reviews-card">
+<img src="../assets/images/courses/review-1.png" alt="Images">
+<h2>Jorge Overall</h2>
+<div class="date">
+<p>jan 17, 2022 at 10:43 am</p>
+</div>
+<div class="icon">
+<i class="ri-star-fill"></i>
+ <i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="single-reviews-card">
+<img src="../assets/images/courses/review-2.png" alt="Images">
+<h2>Manuel Myers</h2>
+<div class="date">
+<p>jan 17, 2022 at 10:43 am</p>
+</div>
+<div class="icon">
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="single-reviews-card">
+<img src="../assets/images/courses/review-1.png" alt="Images">
+<h2>Sally Hatch</h2>
+<div class="date">
+<p>jan 17, 2022 at 10:43 am</p>
+</div>
+<div class="icon">
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+<i class="ri-star-fill"></i>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="review-form">
+<h3>Write a Review</h3>
+<form>
+<div class="row">
+<div class="col-lg-6">
+<div class="form-group">
+<label>Name</label>
+<input type="text" class="form-control" id="name">
+</div>
+ </div>
+<div class="col-lg-6">
+<div class="form-group">
+<label>Email</label>
+<input type="email" class="form-control" id="email">
+</div>
+</div>
+<div class="col-lg-12">
+<div class="form-group">
+<label>Summary</label>
+<textarea class="form-control" id="review" rows="4"></textarea>
+</div>
+</div>
+</div>
+<button type="submit" class="default-btn btn">Submit Review</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="comments">
+<h3>2 comments</h3>
+<div class="single-comments-box">
+<img src="../assets/images/courses/review-1.png" alt="Images">
+<h4>James Benjamin</h4>
+<div class="date">
+<p>jan 17, 2022 at 10:43 am</p>
+</div>
+<div class="reply">
+<a href="#">Reply</a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+<div class="single-comments-box">
+<img src="../assets/images/courses/review-2.png" alt="Images">
+<h4>Elizabeth Sofia</h4>
+<div class="date">
+<p>jan 17, 2022 at 10:43 am</p>
+</div>
+<div class="reply">
+<a href="#">Reply</a>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem incid idunt ut dolore magna aliqua. Ut enim ad minim ven iam quis nostrud </p>
+</div>
+</div>
+<div class="reply-area">
+<div class="reply-form">
+<h3>Leave a Reply</h3>
+<p>Your email address will not be published.</p>
+<form>
+<div class="row">
+<div class="col-lg-12">
+<div class="form-group">
+<textarea class="form-control" id="review2" rows="4" placeholder="Comment"></textarea>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="form-group">
+<input type="text" class="form-control" id="name2" placeholder="Name">
+</div>
+</div>
+<div class="col-lg-4">
+<div class="form-group">
+<input type="email" class="form-control" id="email2" placeholder="Email">
+</div>
+</div>
+<div class="col-lg-4">
+<div class="form-group">
+<input type="text" class="form-control" id="website" placeholder="Website">
+</div>
+</div>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+<label class="form-check-label" for="flexCheckDefault">
+Save my name, email, and website in this browser for the next time I comment.
+</label>
+</div>
+<button type="submit" class="default-btn btn">Post a Comment <i class="flaticon-paper-plane"></i></button>
+</form>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="course-details-right-content">
+<div class="serch-content">
+<h3>Search</h3>
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Find Your Course">
+<button type="submit" class="src-btn">
+<i class="flaticon-search"></i>
+</button>
+</div>
+</div>
+</div>
+<div class="enroll-courses">
+<div class="enroll-img">
+<img src="../assets/images/courses/courses-4.jpg" alt="Image">
+<div class="icon">
+<a class="popup-youtube play-btn" href="https://www.youtube.com/watch?v=6WQCJx_vEX4"><i class="ri-play-fill"></i></a>
+</div>
+</div>
+<div class="list">
+<ul>
+<li><span>Instructor :</span>Lewis</li>
+<li><span>Lectures :</span>12</li>
+<li><span>Duration :</span>20h 41m 32s</li>
+<li><span>Enrolled :</span>2 students</li>
+<li><span>Course level :</span>Intermediate</li>
+<li><span>Language :</span>English</li>
+</ul>
+</div>
+<a href="courses.html" class="default-btn btn">Enroll Courses</a>
+</div>
+<div class="related-download">
+<h3>Related Downloads</h3>
+ <ul>
+<li><a href="#"><i class="flaticon-pdf-file"></i>Brochure Department</a></li>
+<li><a href="#"><i class="flaticon-pdf-file"></i>Department Details</a></li>
+<li><a href="#"><i class="flaticon-pdf-file"></i>Journals Departments</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="footer-area pt-100 pb-70">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-sm-6">
+<div class="footer-logo-area">
+<a href="index.html"><img src="../assets/images/white-logo.png" alt="Image"></a>
+<p>Sanu University was established by J.H Merthon in 1810 for the public benefit. Afterwards, it is recognized globally</p>
+<div class="contact-list">
+<ul>
+<li><a href="tel:+01987655567685">+01-9876-5556-7685
+</a></li>
+<li><a href="/cdn-cgi/l/email-protection#c3a2a7aeaaad83b0a2adb6eda6a7b6"><span class="__cf_email__" data-cfemail="7c1d181115123c0f1d120952191809">[email&#160;protected]</span></a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3>Campus Life</h3>
+<div class="list">
+<ul>
+<li><a href="campus-life.html">Accessibility</a></li>
+<li><a href="campus-life.html">Financial Aid</a></li>
+<li><a href="campus-life.html">Food Services</a></li>
+<li><a href="campus-life.html">Housing</a></li>
+<li><a href="campus-life.html">Information Technologies</a></li>
+<li><a href="campus-life.html">Student Life</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<div class="footer-widjet">
+<h3>Our Campus</h3>
+<div class="list">
+<ul>
+<li><a href="campus-life.html">Acedemic</a></li>
+<li><a href="campus-life.html">Planning & Administration</a></li>
+<li><a href="campus-life.html">Campus Safety</a></li>
+<li><a href="campus-life.html">Office of the Chancellor</a></li>
+<li><a href="campus-life.html">Facility Services</a></li>
+<li><a href="campus-life.html">Human Resources</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-sm-6">
+<div class="footer-widjet">
+<h3>Academics</h3>
+<div class="list">
+<ul>
+<li><a href="academics.html">Canvas</a></li>
+<li><a href="academics.html">Catalyst</a></li>
+<li><a href="academics.html">Library</a></li>
+<li><a href="academics.html">Time Schedule</a></li>
+<li><a href="academics.html">Apply For Admissions</a></li>
+ <li><a href="academics.html">Pay My Tuition</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="shape">
+<img src="../assets/images/shape-1.png" alt="Image">
+</div>
+</div>
+</div>
+
+
+<div class="copyright-area">
+<div class="container">
+<div class="copyright">
+<div class="row">
+<div class="col-lg-6 col-md-4">
+<div class="social-content">
+<ul>
+<li><span>Follow Us On</span></li>
+<li>
+<a href="https://www.facebook.com" target="_blank"><i class="ri-facebook-fill"></i></a>
+</li>
+<li>
+<a href="https://www.twitter.com" target="_blank"><i class="ri-twitter-fill"></i></a>
+</li>
+<li>
+<a href="https://instagram.com/?lang=en" target="_blank"><i class="ri-instagram-line"></i></a>
+</li>
+<li>
+<a href="https://linkedin.com/?lang=en" target="_blank"><i class="ri-linkedin-fill"></i></a>
+</li>
+</ul>
+</div>
+</div>
+<div class="col-lg-6 col-md-8">
+<div class="copy">
+<p>© Sanu is Proudly Owned by <a href="https://hibootstrap.com/" target="_blank">HiBootstrap</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="go-top">
+<i class="ri-arrow-up-s-line"></i>
+<i class="ri-arrow-up-s-line"></i>
+</div>
+
+
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="../assets/js/jquery.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/js/jquery.meanmenu.js"></script>
+
+<script src="../assets/js/owl.carousel.min.js"></script>
+
+<script src="../assets/js/carousel-thumbs.min.js"></script>
+
+<script src="../assets/js/jquery.magnific-popup.js"></script>
+
+<script src="../assets/js/aos.js"></script>
+
+<script src="../assets/js/odometer.min.js"></script>
+
+<script src="../assets/js/appear.min.js"></script>
+
+<script src="../assets/js/form-validator.min.js"></script>
+
+<script src="../assets/js/contact-form-script.js"></script>
+
+<script src="../assets/js/ajaxchimp.min.js"></script>
+
+<script src="../assets/js/custom.js"></script>
+</body>
+</html>
